5352c3457848a156ffd220a0bdb3ee33dbb26526
Guterresthomas <guterresmikael@gmail.com>
2023-08-03 20:09:44 -0300

    aa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import React, {  } from "react"
+import Link from "next/link"
 export function Home () {
   return (
     <div>
-      
+      <div>
+        <h1>Estudos de fetch:</h1>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   )
 }
