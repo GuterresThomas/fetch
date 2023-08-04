1ae2a0bb4c8e29909b2a4bd9f8573adc7ba6df4c
Guterresthomas <guterresmikael@gmail.com>
2023-08-04 17:22:16 -0300

    aa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 export default function Home () {
   return (
-    <div>
-      <div>
+    <div className="mt-10 flex justify-center">
+      <div className="bg-zinc-400 rounded-md p-4">
         <h1>Estudos de fetch:</h1>
         <ul>
-          <Link href="/products"><li>Produtos</li></Link>
+          <Link href="/products"><li className="font-bold hover:scale-105">Produtos</li></Link>
           <Link href=""><li></li></Link>
           <Link href=""><li></li></Link>
           <Link href=""><li></li></Link>
